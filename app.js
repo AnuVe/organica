473f331cde0e279f48d37a69d2eccd5c8865a17b
unknown <ajaditi0311@gmail.com>
2021-02-14 22:41:14 +0530

    Connected to mongodb and shifted plants to db

--- app.js
+++ app.js
@@ -1,37 +1,77 @@
-var express=require("express");
-var app=express();
-var bodyParser=require("body-parser");
+var express = require("express"),
+    app = express(), 
+    bodyParser = require("body-parser"), 
+    mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
-var plants=[
-    {name:"Alovera",image:"https://i.ytimg.com/vi/AHlJuY0bagA/maxresdefault.jpg"}
-]
+require('dotenv').config();
 
+const uri = process.env.MONGO_URL;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+/*var plants=[
+    {name:"Alovera",image:"https://i.ytimg.com/vi/AHlJuY0bagA/maxresdefault.jpg"},
+    {name: "Neem",image: "https://images.unsplash.com/photo-1564505676257-57af8f7e43ab?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8bmVlbSUyMHBsYW50fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"}
+]*/
+
+var plantSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+});
+
+var Plant = mongoose.model("Plant", plantSchema);
+
+/*Plant.create(
+    {
+        name: "Alovera",
+        image: "https://i.ytimg.com/vi/AHlJuY0bagA/maxresdefault.jpg"
+    }, function(err, plant) {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log("Newly created plant");
+            console.log(plant);
+        }
+    });*/
 
 app.get("/",function(req,res){
     res.render("landing");
 });
 
 app.get("/plants",function(req,res){
-    
-    res.render("plants",{plants:plants});
+    Plant.find({}, function(err, allplants){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("plants", {plants: allplants});        
+        }
+    });
 });
 
 app.post("/plants",function(req,res){
     //res.send("POST");
     var name = req.body.name;
     var image = req.body.image;
-    var newplant = {name:name, image:image}
-    plants.push(newplant);
-    res.redirect("/plants");
+    var newPlant = {name:name, image:image}
+    Plant.create(newPlant, function(err, newlyCreated) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/plants");
+        }
+    });
 });
 
 app.get("/plants/new",function(req,res){
     res.render("new.ejs");
 });
 
-app.listen(8000,function(){
-    console.log("Hello");
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
