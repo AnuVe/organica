2372dc8669ce3299a0ff4ddd8ce65c122f52ff87
s-282 <yadav.sonal282@gmail.com>
2021-03-01 19:54:42 +0530

    Added authentication on show part

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ var express = require("express"),
     app = express(), 
     bodyParser = require("body-parser"), 
     mongoose = require("mongoose"),
-    //
     flash = require("connect-flash");
-    //...
-
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
-    //
     methodOverride = require("method-override"),
-    //...
-
     Plant = require("./models/plant"),
     Comment=require("./models/comment"),
     User = require("./models/user.js"),
@@ -24,10 +18,8 @@ var commentRoutes = require("./routes/comments"),
 app.use(bodyParser.urlencoded({extended:true}));
 app.use( express.static( "public" ) );
 app.set("view engine","ejs");
-//
 app.use(methodOverride("_method"));
 app.use(flash());
-//....
 
 require('dotenv').config();
 
@@ -37,8 +29,6 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
-
-//..seedDB();
 
 app.use(require("express-session")({
     secret: "abcd",
@@ -53,154 +43,14 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
-    //
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
-    //.....
-    
     next();
 });
-
-// /*var plants=[
-//     {name:"Alovera",image:"https://i.ytimg.com/vi/AHlJuY0bagA/maxresdefault.jpg"},
-//     {name: "Neem",image: "https://images.unsplash.com/photo-1564505676257-57af8f7e43ab?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8bmVlbSUyMHBsYW50fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"}
-// ]*/
-
-// /*Plant.create(
-//     {
-//         name: "Alovera",
-//         image: "https://i.ytimg.com/vi/AHlJuY0bagA/maxresdefault.jpg"
-//     }, function(err, plant) {
-//         if(err) {
-//             console.log(err)
-//         } else {
-//             console.log("Newly created plant");
-//             console.log(plant);
-//         }
-//     });*/
-
-// app.get("/",function(req,res){
-//     res.render("landing");
-// });
-
-// app.get("/plants",function(req,res){
-//     Plant.find({}, function(err, allplants){
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             res.render("plants/index", {plants: allplants});        
-//         }
-//     });
-// });
-
-// app.post("/plants",function(req,res){
-//     //res.send("POST");
-//     var name = req.body.name;
-//     var image = req.body.image;
-//     var newPlant = {name:name, image:image}
-//     Plant.create(newPlant, function(err, newlyCreated) {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             res.redirect("/plants");
-//         }
-//     });
-// });
-
-// app.get("/plants/new",function(req,res){
-//     res.render("plants/new");
-// });
-
-// app.get("/plants/:id", function(req, res) {
-//     Plant.findById(req.params.id).populate("comments").exec(function(err, foundPlant) {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             res.render("plants/show",{plant: foundPlant});
-//         }
-//     });
-// });
-
-// app.get("/plants/:id/comments/new", isLoggedIn, function(req,res){
-//     Plant.findById(req.params.id, function(err,plant){
-//         if(err){
-//             console.log(err);
-//         } else{
-//             res.render("comments/new",{plant:plant});
-//         }
-//     })
-// });
-
-// app.post("/plants/:id/comments", isLoggedIn, function(req,res){
-//     Plant.findById(req.params.id,function(err,plant){
-//         if(err){
-//             console.log(err);
-//             res.redirect("/plants");
-//         } else{
-//             Comment.create(req.body.comment,function(err,comment){
-//                 if(err){
-//                     console.log(err);
-//                 } else{
-//                     plant.comments.push(comment);
-//                     plant.save();
-//                     res.redirect('/plants/'+plant._id);
-//                 }
-//             });
-//         }
-//     });
-// });
-
-// app.get("/register", function(req, res) {
-//     res.render("register");
-// })
-
-// app.post("/register", function(req,res) {
-//     var newUser = new User({username: req.body.username});
-//     User.register(newUser, req.body.password, function(err, user) {
-//         if(err) {
-//             console.log(err);
-//             return res.render("register")
-//         }
-//         passport.authenticate("local")(req, res, function() {
-//             res.redirect("/plants");
-//         });
-//     });
-// });
-
-// app.get("/login", function(req, res) {
-//     res.render("login");
-// });
-
-// app.post("/login", passport.authenticate("local", 
-//     {
-//         successRedirect: "/plants",
-//         failureRedirect: "/login"
-//     }), function(req, res) {
-// });
-
-// app.get("/logout", function(req, res) {
-//     req.logout();
-//     res.redirect("/plants");
-// });
-
-// function isLoggedIn(req, res, next) {
-//     if(req.isAuthenticated()) {
-//         return next();
-//     }
-//     res.redirect("/login");
-// }
 
 app.use("/",indexRoutes);
 app.use("/plants",plantRoutes);
 app.use("/plants/:id/comments",commentRoutes);
-//
-var plants = [
-    {name: "Great Outdoors",image: "https://images.unsplash.com/photo-1507163525711-618d70c7a8f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name: "Northway", image: "https://images.unsplash.com/photo-1582908140887-5935bade88da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name: "Tripp Lake", image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-];
-//...
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
